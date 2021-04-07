ba17038aa4dd94f35ed9cf8bf2b7267f68ad516a
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-04-07 19:47:35 +0200

    Add image uploader component

--- src/app/modules/image-processing/image-processing.module.ts
+++ src/app/modules/image-processing/image-processing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FileUploaderComponent } from '../../components/file-uploader/file-uploader.component';
+import { ImageUploaderComponent } from '../../components/image-uploader/image-uploader.component';
 import { ImageProcessingComponent } from '../../components/image-processing/image-processing.component';
 import { ImageProcessingRoutingModule } from './image-processing-routing/image-processing-routing.module';
 
 @NgModule({
-  declarations: [FileUploaderComponent, ImageProcessingComponent],
+  declarations: [ImageUploaderComponent, ImageProcessingComponent],
   imports: [
     CommonModule,
     ImageProcessingRoutingModule
